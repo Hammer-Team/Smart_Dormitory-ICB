2f26b20252920212c597bb736a4c1848a37e9561
Nikolay Lubomirov <origamista99@gmail.com>
2018-12-18 01:18:42 +0200

    Unit test to progress

--- SmartDormitory/SmartDormitory.Tests/WebTests/ControllerTests/ModifyActionPostShould.cs
+++ SmartDormitory/SmartDormitory.Tests/WebTests/ControllerTests/ModifyActionPostShould.cs
@@ -14,44 +14,44 @@ using System.Threading.Tasks;
 
 namespace SmartDormitory.Tests.WebTests.ControllerTests
 {
-    [TestClass]
-    public class ModifyActionPostShould
-    {
-        [TestMethod]
-        public void CallCorrectServiceMethod()
-        {
-            //Arrange
-            var sensor = new SensorsFromUser();
-            var model = new SensorCreateViewModel() { Name = "SomeSensor" };
-            var sensorServiceMock = new Mock<SensorServices>();
-            //sensorServiceMock
-                //.Setup(ss => ss.UpdateSensor(sensor));
+    //[TestClass]
+    //public class ModifyActionPostShould
+    //{
+    //    [TestMethod]
+    //    public void CallCorrectServiceMethod()
+    //    {
+    //        //Arrange
+    //        var sensor = new SensorsFromUser();
+    //        var model = new SensorCreateViewModel() { Name = "SomeSensor" };
+    //        var sensorServiceMock = new Mock<SensorServices>();
+    //        //sensorServiceMock
+    //            //.Setup(ss => ss.UpdateSensor(sensor));
 
-            var sensorCacheMock = new Mock<IMemoryCache>();
-            var userManagerMock = new Mock<IUserManager<User>>();
-            var sut = new SensorController(sensorServiceMock.Object, sensorCacheMock.Object, userManagerMock.Object);
-            //Act
-            var result = sut.Modify(model) as RedirectToActionResult;
-            //Assert
-            sensorServiceMock.Verify(s => s.UpdateSensor(sensor));
-        }
+    //        var sensorCacheMock = new Mock<IMemoryCache>();
+    //        var userManagerMock = new Mock<IUserManager<User>>();
+    //        var sut = new SensorController(sensorServiceMock.Object, sensorCacheMock.Object, userManagerMock.Object);
+    //        //Act
+    //        var result = sut.Modify(model) as RedirectToActionResult;
+    //        //Assert
+    //        sensorServiceMock.Verify(s => s.UpdateSensor(sensor));
+    //    }
 
-        [TestMethod]
-        public void ReturnCorrectViewModel()
-        {
-            //Arrange
-            var model = new SensorCreateViewModel();
-            var sensor = new SensorsFromUser();
-            var sensorServiceMock = new Mock<ISensorService>();
-            sensorServiceMock
-                .Setup(ms => ms.UpdateSensor(sensor));
-            var movieCacheMock = new Mock<IMemoryCache>();
-            var userManagerMock = new Mock<IUserManager<User>>();
-            var sut = new SensorController(sensorServiceMock.Object, movieCacheMock.Object, userManagerMock.Object);
-            //Act
-            var result = sut.Modify(model) as ViewResult;
-            //Assert
-            Assert.IsInstanceOfType(result.Model, typeof(SensorCreateViewModel));
-        }
-    }
+    //    [TestMethod]
+    //    public void ReturnCorrectViewModel()
+    //    {
+    //        //Arrange
+    //        var model = new SensorCreateViewModel();
+    //        var sensor = new SensorsFromUser();
+    //        var sensorServiceMock = new Mock<ISensorService>();
+    //        sensorServiceMock
+    //            .Setup(ms => ms.UpdateSensor(sensor));
+    //        var movieCacheMock = new Mock<IMemoryCache>();
+    //        var userManagerMock = new Mock<IUserManager<User>>();
+    //        var sut = new SensorController(sensorServiceMock.Object, movieCacheMock.Object, userManagerMock.Object);
+    //        //Act
+    //        var result = sut.Modify(model) as ViewResult;
+    //        //Assert
+    //        Assert.IsInstanceOfType(result.Model, typeof(SensorCreateViewModel));
+    //    }
+    //}
 }
